3d87222bda479a9ec30be393e9ad66d8cbf7c194
Yusuf ALTUNOK <130599984+yusufaltunok@users.noreply.github.com>
2024-05-07 21:21:10 +0300

    Update ProfilPage.java

--- src/test/java/abonesepeti/pages/ProfilPage.java
+++ src/test/java/abonesepeti/pages/ProfilPage.java
@@ -70,63 +70,6 @@ public class ProfilPage extends ReusableMethods {
 
     @AndroidFindBy(id = "com.abonesepeti.app:id/btn_positive_custom_dialog")
     public WebElement geriBildirimMesaji_Tamam_Button;
- 
-    @AndroidFindBy(id = "com.abonesepeti.app:id/imgProfile")
-    public WebElement profil;
-
-    @AndroidFindBy(id = "com.abonesepeti.app:id/btnNotifications")
-    public WebElement bildirim;
-
-    @AndroidFindBy(id = "com.abonesepeti.app:id/btnNotifications")
-    public WebElement hane1;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Abonelik Ekle\")")
-    public WebElement abonelikEkle ;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Yeni Abonelik\")")
-    public WebElement yeniAbonelik;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Kolay İptal\")")
-    public WebElement kolayIptal;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Fatura Ödeme\")")
-    public WebElement faturaOdeme;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Tüm İşlemler\")")
-    public WebElement tumIslemler;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Cebe TL (Yakında)\")")
-    public WebElement cebeTLYakinda;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Araç Kirala\")")
-    public WebElement aracKirala;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Sigorta Teklifi Al \")")
-    public WebElement sigortaTeklifiAl;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Geleceğim\")")
-    public WebElement gelecegim;
-
-    @AndroidFindBy(uiAutomator="new UiSelector().text(\"Kapat\")")
-    public WebElement kapat;
-
-    @AndroidFindBy(accessibility = "Abonesepeti")
-    public WebElement abonesepetiButton;
-
-    @AndroidFindBy(accessibility = "Abonelikler")
-    public WebElement aboneliklerButton;
-
-    @AndroidFindBy(accessibility = "İşlemler")
-    public WebElement IslemlerButton;
-
-    @AndroidFindBy(accessibility = "Kampanyalar")
-    public WebElement KampanyalarButton;
-
-    @AndroidFindBy(accessibility = "Yönet")
-    public WebElement YonetButton;
-
-    @AndroidFindBy(className = "android.widget.LinearLayout")
-    public List<WebElement> ilkHane;
 
     @AndroidFindBy(id = "com.abonesepeti.app:id/tv_membership_status")
     public WebElement uyelik_button;
