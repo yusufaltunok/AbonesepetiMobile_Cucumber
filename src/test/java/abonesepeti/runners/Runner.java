20774c8626917813f2089a7b9d38ad0c17085915
famugithub <fatihmclskn@gmail.com>
2024-05-19 13:44:21 +0300

    runner duzeltidi

--- src/test/java/abonesepeti/runners/Runner.java
+++ src/test/java/abonesepeti/runners/Runner.java
@@ -3,28 +3,19 @@ package abonesepeti.runners;
 import   io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-<<<<<<< HEAD
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "html:raporlar/CucumberRapor.html",
         features = "src/test/resources/features",
         glue = {"abonesepeti/stepdefs","abonesepeti/hooks"},
-        tags = "@US21",
+        tags = "@us24",
         dryRun = false,
         monochrome = true
-=======
-    @RunWith(Cucumber.class)
-    @CucumberOptions(
-            plugin = "html:raporlar/CucumberRapor.html",
-            features = "src/test/resources/features",
-            glue = {"abonesepeti/stepdefs","abonesepeti/hooks"},
-            tags = "@us24",
-            dryRun = false,
-            monochrome = true
->>>>>>> master
 
 )
 
 
 public class Runner {
 }
+
+
