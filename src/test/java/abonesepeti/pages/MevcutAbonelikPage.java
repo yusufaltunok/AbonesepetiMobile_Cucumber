cbe10c498082c03b26dc17982c03b83c12a89915
emin2323 <meliha2300@gmail.com>
2024-05-21 22:52:22 +0300

    22/05/emin/

---
+++ src/test/java/abonesepeti/pages/MevcutAbonelikPage.java
@@ -0,0 +1,10 @@
+package abonesepeti.pages;
+
+import abonesepeti.utilities.Driver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+public class MevcutAbonelikPage {
+    public MevcutAbonelikPage() { PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);}
+
+}
---
+++ src/test/java/abonesepeti/stepdefs/abonelikler/MevcutAboneliklerStepDefs.java
@@ -0,0 +1,11 @@
+package abonesepeti.stepdefs.abonelikler;
+
+import abonesepeti.pages.AnasayfaPage;
+import abonesepeti.utilities.ReusableMethods;
+
+public class MevcutAboneliklerStepDefs extends ReusableMethods {
+
+    MevcutAboneliklerStepDefs mevcutAboneliklerStepDefs = new MevcutAboneliklerStepDefs();
+    AnasayfaPage anasayfaPage = new AnasayfaPage();
+
+}
