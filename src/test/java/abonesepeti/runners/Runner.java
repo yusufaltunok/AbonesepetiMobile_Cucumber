4418464e439e6bac5a35639cd023b9288585f511
yusufaltunok <yusuf.66.77@gmail.com>
2024-05-27 22:47:08 +0300

    yusuf_27.5

--- src/test/java/abonesepeti/runners/Runner.java
+++ src/test/java/abonesepeti/runners/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
             plugin = "html:raporlar/CucumberRapor.html",
             features = "src/test/resources/features",
             glue = {"abonesepeti/stepdefs","abonesepeti/hooks"},
-            tags = "@us24",
+            tags = "@US25_TC01",
             dryRun = false,
             monochrome = true
 
